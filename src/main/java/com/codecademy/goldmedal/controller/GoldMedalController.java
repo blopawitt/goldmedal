e0e893f8e441146466d0c9d05e8f81f7edd34730
blopawitt <pablowitt321@gmail.com>
2022-10-26 23:28:44 -0300

    getCountryMedalsListResponse

--- src/main/java/com/codecademy/goldmedal/controller/GoldMedalController.java
+++ src/main/java/com/codecademy/goldmedal/controller/GoldMedalController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/countries")
 public class GoldMedalController {
-    // TODO: declare references to your repositories (ok)
+
     private final GoldMedalRepository goldMedalRepository;
     private final CountryRepository countryRepository;
     public GoldMedalController(final GoldMedalRepository goldMedalRepository, final CountryRepository countryRepository){
@@ -45,19 +45,19 @@ public class GoldMedalController {
         List<GoldMedal> medalsList;
         switch (sortBy) {
             case "year":
-                medalsList = // TODO: list of medals sorted by year in the given order
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByYearAsc(countryName) : goldMedalRepository.getByCountryOrderByYearDesc(countryName);
                 break;
             case "season":
-                medalsList = // TODO: list of medals sorted by season in the given order
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderBySeasonAsc(countryName) : goldMedalRepository.getByCountryOrderBySeasonDesc(countryName);
                 break;
             case "city":
-                medalsList = // TODO: list of medals sorted by city in the given order
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByCityAsc(countryName) : goldMedalRepository.getByCountryOrderByCityDesc(countryName);
                 break;
             case "name":
-                medalsList = // TODO: list of medals sorted by athlete's name in the given order
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByNameAsc(countryName) : goldMedalRepository.getByCountryOrderByNameDesc(countryName);
                 break;
             case "event":
-                medalsList = // TODO: list of medals sorted by event in the given order
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByEventAsc(countryName) : goldMedalRepository.getByCountryOrderByEventDesc(countryName);
                 break;
             default:
                 medalsList = new ArrayList<>();
--- src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
+++ src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
@@ -2,7 +2,17 @@ package com.codecademy.goldmedal.repositories;
 
 import com.codecademy.goldmedal.model.GoldMedal;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface GoldMedalRepository extends CrudRepository<GoldMedal, Integer> {
-
+    List<GoldMedal> getByCountryOrderByYearAsc(String country);
+    List<GoldMedal> getByCountryOrderByYearDesc(String country);
+    List<GoldMedal> getByCountryOrderBySeasonAsc(String season);
+    List<GoldMedal> getByCountryOrderBySeasonDesc(String season);
+    List<GoldMedal> getByCountryOrderByCityAsc(String city);
+    List<GoldMedal> getByCountryOrderByCityDesc(String city);
+    List<GoldMedal> getByCountryOrderByNameAsc(String name);
+    List<GoldMedal> getByCountryOrderByNameDesc(String name);
+    List<GoldMedal> getByCountryOrderByEventAsc(String event);
+    List<GoldMedal> getByCountryOrderByEventDesc(String event);
 }
